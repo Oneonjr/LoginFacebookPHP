c56a0903a0f74c8d7d83743891744b935dfa45c4
Oneon Junior <oneonjunior@outlook.com>
2023-12-17 22:15:04 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+	
+	session_start();
+
+	include('facebook-php-sdk/autoload.php');
+	use Facebook\Facebook;
+	use Facebook\Exceptions\FacebookResponseException;
+	use Facebook\Exceptions\FacebookSDKException;
+
+	$appId = '1685805781906061';
+	$appSecret = '0b7966b2c7dc747305edb8012741bfee';
+	$redirectUrl = 'http://localhost/danki/login-facebook/';
+	$fbPermission = array('email');
+
+	$fb = new Facebook(array(
+		'app_id'=> $appId,
+		'app_secret'=> $appSecret,
+		'default_graph_version' => 'v2.2'
+	));
+
+	$helper = $fb->getRedirectLoginHelper();
+
+	try{
+		if(isset($_SESSION['facebook_access_token'])){
+			$accessToken = $_SESSION['facebook_access_token'];
+		}else{
+			$accessToken = $helper->getAccessToken();
+		}
+	}catch(FacebookResponseException $e){}
+
+?>
